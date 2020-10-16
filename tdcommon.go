7085003b814bed0f5badbd914d5cc5710195a143
秦晓伟 <sayyoume@gmail.com>
2020-10-16 15:00:11 +0800

    Update tdcommon.go
    增加进制转换

--- tdcommon.go
+++ tdcommon.go
@@ -1,6 +1,7 @@
 package td
 
 import (
+	"encoding/hex"
 	"math/rand"
 	"time"
 )
@@ -9,4 +10,19 @@ var r = rand.New(rand.NewSource(time.Now().UnixNano()))
 func GenerateRangeNum(min, max int) int {
 	randNum := r.Intn(max - min) + min
 	return randNum
+}
+
+func TdEncode(str string){
+	//字符转换16进制
+	src := []byte(str)
+	encodedStr := hex.EncodeToString(src)
+	// [72 101 108 108 111]
+	//fmt.Println(src)
+	fmt.Println(encodedStr)
+}
+
+func TdDecode(str string){
+	//16进制转换字符串
+	decodestr ,_:= hex.DecodeString(str)
+	fmt.Println(string(decodestr))
 }
